62a8c928f1c01dfedaf23f92d63a15ed9d823d5c
Vladimir Deushev <1991rand@gmail.com>
2024-03-18 14:25:43 +0300

    feat: add API POM test

--- cypress/e2e/sausedemo-test/api.cy.js
+++ cypress/e2e/sausedemo-test/api.cy.js
@@ -1,58 +1,27 @@
 /// <reference types='cypress' />
+import { url, body } from "../../../PageObjects/PageElements/APIPageElements.json";
 
 describe("CY API test", () => {
   it("Find pet by ID", () => {
-    cy.request("GET", "https://petstore.swagger.io/v2/pet/5").should(
-      (response) => {
-        expect(response.status).to.eq(200);
-      }
-    );
+    cy.request("GET", `${url}/8`).should((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 
   it("Add a new pet to the store", () => {
-    cy.request("POST", "https://petstore.swagger.io/v2/pet", {
-      id: 0,
-      category: {
-        id: 0,
-        name: "string",
-      },
-      name: "doggie",
-      photoUrls: ["string"],
-      tags: [
-        {
-          id: 0,
-          name: "string",
-        },
-      ],
-      status: "available",
-    }).should((response) => {
+    cy.request("POST", `${url}`, body).should((response) => {
       expect(response.status).to.eq(200);
     });
   });
 
   it("Update an existing pet", () => {
-    cy.request("PUT", "https://petstore.swagger.io/v2/pet", {
-      id: 0,
-      category: {
-        id: 0,
-        name: "string",
-      },
-      name: "doggie",
-      photoUrls: ["string"],
-      tags: [
-        {
-          id: 0,
-          name: "string",
-        },
-      ],
-      status: "available",
-    }).should((response) => {
+    cy.request("PUT", `${url}`, body).should((response) => {
       expect(response.status).to.eq(200);
     });
   });
 
   it("Delete a pet", () => {
-    cy.request("DELETE", "https://petstore.swagger.io/v2/pet/1").should(
+    cy.request("DELETE", `${url}/7`).should(
       (response) => {
         expect(response.status).to.eq(200);
       }
