d952b4800686b11ad46653f51248080401344ae2
Vladimir Deushev <1991rand@gmail.com>
2024-03-26 12:03:09 +0300

    add reporter mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -8,4 +8,15 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    // To display small circular charts regarding test results
+    charts: true,
+    // Generate JSON file to create custom reports
+    json: true,
+    // Customize the report file name
+    reportFilename: 'my-report',
+    // Generate new report file or overwrite the a single file
+    overwrite: false
+  }  
 });
